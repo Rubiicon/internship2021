6ba9b62e70f70bb48de323caa5ae8c8062549191
Rubick <rubick.ua@gmail.com>
2021-06-17 13:12:28 +0300

    promise2

--- week1/promises.js
+++ week1/promises.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-/* const getUsers = () => {
+const getUsers = () => {
     return new Promise((resolve, reject) => {
         request(
             'https://jsonplaceholder.typicode.com/users',
@@ -19,11 +19,11 @@ getUsers()
     })
     .catch((error) => {
         throw new Error(error);
-    }); */
+    });
 
 // 1. Rewrite getUsers to async/await
 
-/* const getUsers2 = async () => {
+const getUsers2 = async () => {
     await request(
         'https://jsonplaceholder.typicode.com/users',
         (error, response, body) => {
@@ -35,36 +35,37 @@ getUsers()
 };
 
 getUsers2();
- */
+
 /**
  *  2. Add another request to url - https://jsonplaceholder.typicode.com/comments.
  *  Make two requests, and return all values
  *  Hint: Promise.all
  * */
 
-const getTwoRequest = () => {
-    Promise.all([
-        new Promise((resolve, reject) => {
-            request(
-                'https://jsonplaceholder.typicode.com/users',
-                (error, response, body) => {
-                    if (error || response.statusCode !== 200) reject(error);
+const promiseA = new Promise((resolve, reject) => {
+    request(
+        'https://jsonplaceholder.typicode.com/users',
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) reject(error);
 
-                    resolve(JSON.parse(body));
-                }
-            );
-        }),
-        new Promise((resolve, reject) => {
-            request(
-                'https://jsonplaceholder.typicode.com/comments',
-                (error, response, body) => {
-                    if (error || response.statusCode !== 200) reject(error);
+            resolve(JSON.parse(body));
+        }
+    );
+});
 
-                    resolve(JSON.parse(body));
-                }
-            );
-        }),
-    ])
+const promiseB = new Promise((resolve, reject) => {
+    request(
+        'https://jsonplaceholder.typicode.com/comments',
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) reject(error);
+
+            resolve(JSON.parse(body));
+        }
+    );
+});
+
+const promiseAll = () => {
+    Promise.all([promiseA, promiseB])
         .then((data) => {
             console.log(data);
         })
@@ -73,14 +74,38 @@ const getTwoRequest = () => {
         });
 };
 
-getTwoRequest();
+promiseAll();
 
 /**
  * 3. Read about Promise.race
  * Try to use it on previous two requests
  */
 
+const promiseRace = () => {
+    Promise.race([promiseA, promiseB])
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            throw new Error(error);
+        });
+};
+
+promiseRace();
+
 /**
  * 4. Read about Promise.any
  * Try to use it on previous two requests
  */
+
+/* const promiseAny = () => {
+    Promise.any([promiseA, promiseB])
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            throw new Error(error);
+        });
+};
+
+promiseAny(); */
